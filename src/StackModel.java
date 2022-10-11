bddf9cdf70ee780a38ce8314224f74139d480ade
LuckyOwl-CN <516342785@qq.com>
2022-10-11 21:08:08 +0800

    Code framework
    first commit

---
+++ src/StackModel.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class StackModel {
+    private Stack<Character> optr = new Stack<>();
+    private Stack<Integer> opnd = new Stack<>();
+    private String expression = null;
+    private char curr;
+
+    //清空栈、调用expression
+    public void _init(){
+        getExpressoion();
+    }
+
+    //获得用户输入
+    private void getExpressoion(){
+
+    }
+
+    //获取字符并做出相应处理
+    private char getCurr(){
+
+    }
+
+    //比较栈内外优先级
+    private Boolean comparePriority(){
+
+    }
+
+
+}
