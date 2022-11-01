9a4f39a92b1ce5e2d4d47bfbdf37073f26aff316
LuckyOwl-CN <516342785@qq.com>
2022-11-01 17:08:33 +0800

    Calculator bug fixed version 2

--- src/UIModel.java
+++ src/UIModel.java
@@ -1,9 +1,41 @@
-public class UIModel {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class UIModel {
+    private static String expression;
+    private static String judge;
+    private static Scanner scanner = new Scanner(System.in);
+
+
+    public void expressionLoad() {
+        System.out.print("Please input expression:");
+        expression = scanner.nextLine();
+    }
+
+    public static String getExpression(){
+        return expression;
+    }
+
+    public static void main(String[] args) {
+        boolean calculate = true;
+        System.out.println("Welcome to use Calculator");
+        UIModel uiModel = new UIModel();
         StackModel stackModel = new StackModel();
         //UI端初始化
-        stackModel._init();
-
+        while (calculate) {
+            uiModel.expressionLoad();
+            stackModel._init();
+            System.out.print("Do you want to calculate again?(Y/N):");
+            judge = scanner.nextLine();
+            while(!judge.equals("Y") && !judge.equals("N")){
+                System.out.print("Do you want to calculate again?(Y/N):");
+                judge = scanner.nextLine();
+            }
+            if(judge.equals("Y") ){
+                calculate = true;
+            }
+            else{
+                calculate = false;
+            }
+        }
     }
 }
